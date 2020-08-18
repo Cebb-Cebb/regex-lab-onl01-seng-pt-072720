53a776a38c77e9fd759e03af94b452f874a045a2
hersson ceballos <herssonceballos@gmail.com>
2020-08-18 01:53:19 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,5 @@
 def starts_with_a_vowel?(word)
-   word{
-    return ['a', 'e', 'i', 'o', 'u'].indexOf(s[0].toLowerCase()) !== -1
-}
-  end
-end 
+   
 
 def words_starting_with_un_and_ending_with_ing(text)
 
